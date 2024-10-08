536cb97d23c07b69889f3f9d204594a6f3dcdc2d
airluof <etofleks@gmail.com>
2024-10-08 21:51:50 +0300

    Update warp-generator.sh

--- warp-generator.sh
+++ warp-generator.sh
@@ -22,21 +22,13 @@ client_ipv6=$(echo "$response" | jq -r '.result.config.interface.addresses.v6')
 port=$(echo "$peer_endpoint" | sed 's/.*:\([0-9]*\)$/\1/')
 peer_endpoint=$(echo "$peer_endpoint" | sed 's/\(.*\):[0-9]*/162.159.193.5/')
 
+# Генерация конфигурационного файла
 conf=$(cat <<-EOM
 [Interface]
 PrivateKey = ${priv}
-S1 = 0
-S2 = 0
-Jc = 120
-Jmin = 23
-Jmax = 911
-H1 = 1
-H2 = 2
-H3 = 3
-H4 = 4
 Address = ${client_ipv4}, ${client_ipv6}
-DNS = 1.1.1.1, 2606:4700:4700::1111, 1.0.0.1, 2606:4700:4700::1001
-
+DNS = 1.1.1.1
+...
 [Peer]
 PublicKey = ${peer_pub}
 AllowedIPs = 0.0.0.0/0, ::/0
@@ -44,11 +36,10 @@ Endpoint = ${peer_endpoint}:${port}
 EOM
 )
 
-clear
-echo -e "\n\n\n"
-[ -t 1 ] && echo "########## НАЧАЛО КОНФИГА ##########"
+# ВЫВОД КОНФИГА НА ЭКРАН
 echo "${conf}"
-[ -t 1 ] && echo "########### КОНЕЦ КОНФИГА ###########"
 
+# Кодирование и создание ссылки для скачивания
 conf_base64=$(echo -n "${conf}" | base64 -w 0)
 echo "Скачать конфиг файлом: https://immalware.github.io/downloader.html?filename=WARP.conf&content=${conf_base64}"
+
