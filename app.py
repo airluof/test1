42b3c0cfd6f6385f742e262a0b781babc407bcaa
airluof <etofleks@gmail.com>
2024-10-08 23:08:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
+import os
 from http.server import SimpleHTTPRequestHandler
 import socketserver
 
-class CustomHandler(SimpleHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == "/download":
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html; charset=utf-8')
-            self.end_headers()
-            # Отправляем HTML-код с ссылкой на скачивание
-            self.wfile.write('<html><body><a href="/WARP.conf">Скачать WARP.conf</a></body></html>'.encode('utf-8'))
-        else:
-            super().do_GET()
+PORT = int(os.getenv("PORT", 8080))  # Использует переменную среды PORT или 8080 по умолчанию
 
-PORT = 8080
-with socketserver.TCPServer(("", PORT), CustomHandler) as httpd:
+Handler = SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print(f"Serving at port {PORT}")
     httpd.serve_forever()
