3a25af17e0103259ee8bc98e9449d3dbb336730d
airluof <etofleks@gmail.com>
2024-10-09 15:52:58 +0300

    Update warp-generator.sh

--- warp-generator.sh
+++ warp-generator.sh
@@ -5,21 +5,18 @@ mkdir -p ~/.cloudshell && touch ~/.cloudshell/no-apt-get-warning
 echo "Установка зависимостей..."
 sudo apt-get update -y --fix-missing && sudo apt-get install wireguard-tools -y --fix-missing
 
-# Генерация приватного и публичного ключа
+# Генерация приватного и публичного ключей
 priv="${1:-$(wg genkey)}"
 pub="${2:-$(echo "${priv}" | wg pubkey)}"
-
-# API Cloudflare
 api="https://api.cloudflareclient.com/v0i1909051800"
+
 ins() { curl -s -H 'user-agent:' -H 'content-type: application/json' -X "$1" "${api}/$2" "${@:3}"; }
 sec() { ins "$1" "$2" -H "authorization: Bearer $3" "${@:4}"; }
 
-# Регистрация клиента
 response=$(ins POST "reg" -d "{\"install_id\":\"\",\"tos\":\"$(date -u +%FT%T.000Z)\",\"key\":\"${pub}\",\"fcm_token\":\"\",\"type\":\"ios\",\"locale\":\"en_US\"}")
-
-# Извлечение данных из ответа
 id=$(echo "$response" | jq -r '.result.id')
 token=$(echo "$response" | jq -r '.result.token')
+
 response=$(sec PATCH "reg/${id}" "$token" -d '{"warp_enabled":true}')
 peer_pub=$(echo "$response" | jq -r '.result.config.peers[0].public_key')
 peer_endpoint=$(echo "$response" | jq -r '.result.config.peers[0].endpoint.host')
@@ -48,6 +45,6 @@ echo -e "\n\n\n"
 echo "${conf}"
 [ -t 1 ] && echo "########### КОНЕЦ КОНФИГА ###########"
 
-# Генерация ссылки для скачивания
+# Кодирование конфигурации в base64
 conf_base64=$(echo -n "${conf}" | base64 -w 0)
 echo "Скачать конфиг файлом: https://immalware.github.io/downloader.html?filename=WARP.conf&content=${conf_base64}"
