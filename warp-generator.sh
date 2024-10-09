005c7d3352d4a29b7a1f03df718cc73dcf16380f
airluof <etofleks@gmail.com>
2024-10-09 16:05:22 +0300

    Update warp-generator.sh

--- warp-generator.sh
+++ warp-generator.sh
@@ -3,9 +3,9 @@
 clear
 mkdir -p ~/.cloudshell && touch ~/.cloudshell/no-apt-get-warning
 echo "Установка зависимостей..."
-sudo apt-get update -y --fix-missing && sudo apt-get install wireguard-tools -y --fix-missing
+sudo apt-get update -y --fix-missing && sudo apt-get install wireguard-tools jq curl -y --fix-missing
 
-# Генерируем приватный и публичный ключи
+# Генерируем приватный ключ
 priv=$(wg genkey)
 pub=$(echo "${priv}" | wg pubkey)
 
@@ -18,10 +18,25 @@ sec() { ins "$1" "$2" -H "authorization: Bearer $3" "${@:4}"; }
 # Регистрация клиента
 response=$(ins POST "reg" -d "{\"install_id\":\"\",\"tos\":\"$(date -u +%FT%T.000Z)\",\"key\":\"${pub}\",\"fcm_token\":\"\",\"type\":\"ios\",\"locale\":\"en_US\"}")
 
+# Проверка успешности запроса
+if [ "$(echo "$response" | jq -r '.success')" != "true" ]; then
+    echo "Ошибка при регистрации: $response"
+    exit 1
+fi
+
 # Извлечение необходимых данных из ответа
 id=$(echo "$response" | jq -r '.result.id')
 token=$(echo "$response" | jq -r '.result.token')
+
 response=$(sec PATCH "reg/${id}" "$token" -d '{"warp_enabled":true}')
+
+# Проверка успешности обновления
+if [ "$(echo "$response" | jq -r '.success')" != "true" ]; then
+    echo "Ошибка при активации WARP: $response"
+    exit 1
+fi
+
+# Извлечение данных конфигурации
 peer_pub=$(echo "$response" | jq -r '.result.config.peers[0].public_key')
 peer_endpoint=$(echo "$response" | jq -r '.result.config.peers[0].endpoint.host')
 client_ipv4=$(echo "$response" | jq -r '.result.config.interface.addresses.v4')
@@ -42,6 +57,12 @@ AllowedIPs = 0.0.0.0/0, ::/0
 Endpoint = ${peer_endpoint}:${port}
 EOM
 )
+
+# Проверка на пустоту конфигурации
+if [[ -z "$peer_pub" || -z "$client_ipv4" || -z "$client_ipv6" || -z "$priv" ]]; then
+    echo "Ошибка: один или несколько ключей пустые."
+    exit 1
+fi
 
 clear
 echo -e "\n\n\n"
